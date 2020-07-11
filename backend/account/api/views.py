6c9979a5e9d8a0dac51c7941f41388246763e86a
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2020-07-11 11:31:29 +0300

    registration

--- backend/account/api/views.py
+++ backend/account/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from account.models import Account
+from search.models import Search
 from account.api.serializers import RegistrationSerializer
 from gamification.models import GameItem
 from rest_framework.response import Response
@@ -10,12 +11,9 @@ class RegistrationView(generics.GenericAPIView):
     serializer_class =  RegistrationSerializer
 
     def post(self, request):
-        print(request.POST)
-        serializer = RegistrationSerializer(request.POST)
+        serializer = RegistrationSerializer(data = request.POST)
         if serializer.is_valid():
-            u = Account.objects.create_user(serializer)
-            g = GameItem.objects.create()
-            u.games.add(g)
+            serializer.save()
             return Response(status = status.HTTP_201_CREATED)
         else:
             return Response(status = status.HTTP_400_BAD_REQUEST)
